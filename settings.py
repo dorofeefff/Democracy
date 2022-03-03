7e43b45026f653378a157b04b4e04ce69a8d9f09
sss <aleksandr.dorofeefff@gmail.com>
2022-03-03 17:57:02 -0500

    modify feedback page

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ SESSION_CONFIGS = [
         name='dem',
         display_name="Democracy",
         app_sequence=[
-            #'dem',
+            'dem',
             'dem_survey', 'payment_info'],
         num_demo_participants=3,
         use_browser_bots=False,
@@ -20,7 +20,7 @@ SESSION_CONFIGS = [
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=1/500, participation_fee=10.00, doc=""
+    real_world_currency_per_point=1/350, participation_fee=10.00, doc=""
 )
 
 PARTICIPANT_FIELDS = []
