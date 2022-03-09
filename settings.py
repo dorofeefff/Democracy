1ca27b553e8d5bbfd2d1ca748da5a3717560d2ca
sss <aleksandr.dorofeefff@gmail.com>
2022-03-09 11:21:47 -0500

    fast

--- settings.py
+++ settings.py
@@ -57,7 +57,7 @@ ROOMS = [
 
 ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
-ADMIN_PASSWORD = environ.get('adminpass')
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
 DEMO_PAGE_INTRO_HTML = """
 Here are some oTree games.
